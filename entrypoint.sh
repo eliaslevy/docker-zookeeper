245fa2407e5f7a77762c822b5e67800ab7eed232
Elias Levy <fearsome.lucidity@gmail.com>
2015-10-13 16:46:34 -0700

    Move static ZK config to a file.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,11 @@
 #!/bin/sh
 
-echo ${MYID:-1} > /tmp/zookeeper/myid
-
-# based on https://github.com/apache/zookeeper/blob/trunk/conf/zoo_sample.cfg
-cat > /opt/zookeeper/conf/zoo.cfg <<EOF
-tickTime=2000
-initLimit=10
-syncLimit=5
-dataDir=/tmp/zookeeper
-clientPort=2181
-quorumListenOnAllIPs=true
-EOF
+echo ${MYID:-1} > /zookeeper/data/myid
 
 # server.1=...
 if [ -n "$SERVERS" ]; then
-    python -c "print '\n'.join(['server.%i=%s:2888:3888' % (i + 1, x) for i, x in enumerate('$SERVERS'.split(','))])" >> /opt/zookeeper/conf/zoo.cfg
+  python -c "print '\n'.join(['server.%i=%s:2888:3888' % (i + 1, x) for i, x in enumerate('$SERVERS'.split(','))])" >> /zookeeper/conf/zoo.cfg
 fi
 
+cd /zookeeper
 exec "$@"
